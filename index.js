fc6e5e830424926390aba31a066c1f4fb96637e8
J-C-O <47179422+J-C-O@users.noreply.github.com>
2019-01-30 19:27:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express'); 
+var app = express();
+const PORT = process.env.PORT || 8080;
+
+var buecher = [
+{autor: "Martin Janus",titel: "Sakrileg",	Beschreibung: "Das beste Buch überhaupt!" },
+{autor: "Stefan Oberland",titel: "Roman",	Beschreibung: "Der beste Roman überhaupt!"},
+{autor: "Julius Thiele", titel: "Java-Kochbuch", Beschreibung: "Das beste Buch zum kochen mit java überhaupt!"}
+];
+
+app.set("viewengine", "ejs");
+app.get("/",function(req,res){
+res.render("buecher.ejs", {buecher}); });
+ 
+app.use(function(request, response) {  // Fehlerbehandlung 
+response.status(404).send("Seite nicht gefunden");
+}); 
+var server = app.listen(PORT, function () {
+   var host = server.address().address
+   var port = server.address().port
+      console.log("Example app listening at http://%s:%s", host, port)
+}); 
